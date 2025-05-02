97a1a5b2ab59f880e3aa1c43a299ccb2bdd7189d
smitepaladin <darktemper20701@gmail.com>
2025-05-02 18:29:28 +0900

    5/2 전종익
    로그인페이지 추가

--- team4shoeshop/lib/view/login.dart
+++ team4shoeshop/lib/view/login.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
+import 'package:team4shoeshop/view/adminlogin.dart';
+import 'package:team4shoeshop/view/joincustomer.dart';
+import 'package:team4shoeshop/view/shoeslistpage.dart';
 
 class Login extends StatefulWidget {
   const Login({super.key});
@@ -8,8 +13,124 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
+
+    // Property
+  late TextEditingController userIdController;
+  late TextEditingController passwordController;
+
+  final box = GetStorage();
+
+  @override
+  void initState() {
+    super.initState();
+    userIdController = TextEditingController();
+    passwordController = TextEditingController();
+    // 초기화
+    initStorage();
+  }
+
+  initStorage(){ 
+    box.write('p_userId', ""); // 초기값을 비어있는걸로 지정
+    box.write('p_password', "");
+  }
+
+  @override
+  void dispose() {
+    disposeStorage(); // 앱 종료할 때 지우기
+    super.dispose();
+  }
+
+  disposeStorage(){
+    box.erase();
+  }
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+  return Scaffold(
+      appBar: AppBar(
+        title: Text('Log In'),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            CircleAvatar(
+              backgroundImage: AssetImage('images/login.png'),
+              radius: 70,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: TextField(
+                controller: userIdController,
+                decoration: InputDecoration(
+                  labelText: '사용자 ID를 입력하세요'
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: TextField(
+                controller: passwordController,
+                decoration: InputDecoration(
+                  labelText: '패스워드를 입력하세요'
+                ),
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                if(userIdController.text.trim().isEmpty || passwordController.text.trim().isEmpty){
+                  errorSnackBar();
+                }else{
+                  _showDialog();
+                }
+              }, 
+              child: Text('Log In'),
+            ),
+            ElevatedButton(
+              onPressed: () => Get.to(Joincustomer()), 
+              child: Text('회원가입'),
+            ),
+            ElevatedButton(
+              onPressed: () => Get.to(Adminlogin()), 
+              child: Text('관리자 페이지'),
+            ),
+          ],
+        ),
+      ),
+    );
+  } // build
+
+  // --- Functions ---
+  errorSnackBar(){
+    Get.snackbar(
+      '경고', 
+      '사용자 ID와 암호를 입력하세요',
+      snackPosition: SnackPosition.TOP,
+      duration: Duration(seconds: 2),
+      colorText: Theme.of(context).colorScheme.onError,
+      backgroundColor: Theme.of(context).colorScheme.error
+    );
   }
-}
+
+  _showDialog(){
+    Get.defaultDialog(
+      title: '환영합니다',
+      middleText: '신분이 확인되었습니다.',
+      backgroundColor: Theme.of(context).colorScheme.primaryContainer,
+      barrierDismissible: false,
+      actions: [
+        TextButton(
+          onPressed: () {
+            Get.back(); // 다이얼로그 지우기
+            saveStorage(); // Id, Password 저장
+            Get.to(Shoeslistpage()); // 화면 이동
+          }, 
+          child: Text('Exit'),
+        ),
+      ]
+    );
+  }
+
+  saveStorage(){
+    box.write('p_userId', userIdController.text);
+  }
+  
+} // class
---
+++ team4shoeshop/lib/view/shoeslistpage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Shoeslistpage extends StatefulWidget {
+  const Shoeslistpage({super.key});
+
+  @override
+  State<Shoeslistpage> createState() => _ShoeslistpageState();
+}
+
+class _ShoeslistpageState extends State<Shoeslistpage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
