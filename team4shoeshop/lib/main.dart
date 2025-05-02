f7d6a3172c40afcb6880a1f135c75bea1407a24c
smitepaladin <darktemper20701@gmail.com>
2025-05-02 17:25:27 +0900

    5/2 전종익
    sqlite 버전 다운그레이드

--- team4shoeshop/lib/main.dart
+++ team4shoeshop/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:team4shoeshop/%08view/login.dart';
+import 'package:get/get.dart';
+import 'package:team4shoeshop/view/login.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,7 +11,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
